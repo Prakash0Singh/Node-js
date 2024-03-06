a77610d50051129c3d3abcb536aadf1d64cbd8aa
Prakash Singh Basnal <prakash0basnal0@gmail.com>
2024-03-06 23:29:42 +0530

    06-March

---
+++ routes/shop.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const express = require('express');
+
+const adminData = require('./admin');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    // console.log('shop.js', adminData.products);
+    // res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
+    res.render('shop')
+});
+
+module.exports = router;
