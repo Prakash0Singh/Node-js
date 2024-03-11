0ab09ffdae4da1c4822d02336e7b4d160169a36b
Prakash Singh Basnal <prakash0basnal0@gmail.com>
2024-03-11 23:25:09 +0530

    main

--- models/product.js
+++ models/product.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const products = []
 
 module.exports = class Product {
@@ -6,10 +8,28 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this)
+        const p = path.join(path.dirname(__dirname), 'data', 'product.json');
+        fs.readFile(p, (err, fileContent) => {
+            let prod = [];
+            if (!err) {
+                prod = JSON.parse(fileContent)
+            }
+            prod.push(this);
+            fs.writeFile(p, JSON.stringify(prod), (err) => {
+                console.log(err)
+            })
+        })
+        // products.push(this)
     }
 
     static fetchAll() {
+        const p = path.join(path.dirname(process.main.filename), 'data', 'product.json');
+        fs.readFile(p, (err, fileContent) => {
+            if (err) {
+                return []
+            }
+            return JSON.parse(fileContent)
+        })
         return products
     }
 }
